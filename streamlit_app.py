94b1c8221178d294ad6618e2cbd3e867fd7d60ef
professoropp <99960496+professoropp@users.noreply.github.com>
2023-02-02 16:57:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
+
 streamlit.title('My Parent New Healthy Diner')
+
 streamlit.header('Menu Breakfast')
 streamlit.text('Omega 3 & Blueberry Oatmeal ')
 streamlit.text('Kale, Spinach & Rocket Smothie ')
