79685d0bc43c5d9a642bd44542ed30d4f30f0bc9
professoropp <99960496+professoropp@users.noreply.github.com>
2023-02-02 23:04:35 +0530

    Update streamlit_app.py
    pandas and menu ulpoaded

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,13 @@ import streamlit
 streamlit.title('My Parent New Healthy Diner')
 
 streamlit.header('Menu Breakfast')
-streamlit.text('Omega 3 & Blueberry Oatmeal ')
-streamlit.text('塞Kale, Spinach & Rocket Smothie ')
-streamlit.text('Hard-Boiled Free-Range Egg ')
-streamlit.text('陋陋 Avacado Toast ')
+streamlit.text('🌹🌹Omega 3 & Blueberry Oatmeal ')
+streamlit.text('🍒Kale, Spinach & Rocket Smothie ')
+streamlit.text('🥚🥚Hard-Boiled Free-Range Egg ')
+streamlit.text('🥑🥑 Avacado Toast ')
 
-streamlit.header(' Build Your Own Fruit Smothie ')
+streamlit.header('🍑🍌Build Your Own Fruit Smothie ')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
