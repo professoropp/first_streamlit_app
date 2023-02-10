b02914d60c90e1468247b0269a0b0d72a7eb6fa3
professoropp <99960496+professoropp@users.noreply.github.com>
2023-02-10 23:37:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ streamlit.stop()
 
 #snowflake-relatedfunctions
 def get_fruit_load_list():
-  ``with  my_cnx.cursor() as my_cur:
+    with  my_cnx.cursor() as my_cur:
           my_cur.execute("select * from fruit_load_list")
           return my_data_rows = my_cur.fetchall()
       
